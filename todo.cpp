0c60a63b8e5ce4be73585382fa9ab408b5c1359b
AtsushiakaRaju <harshasunu2005@gmail.com>
2024-09-11 18:43:41 +0530

    Completed the project and working all good

--- todo.cpp
+++ todo.cpp
@@ -1,94 +1,117 @@
 #include<iostream>
-#include<string>
-#include<fstream>
+#include<string>   //For string
+#include<fstream>  //For file handling
 using namespace std;
-struct node{
+
+int n = 0;  //Variable to store the number of tasks
+
+struct node{      //Create a node structure
+    int i;               
     string task;
     string CompletionTime;
     node* next;
-};
-struct node *head = NULL, *cur,*newnode;
-void create(){
+}; //Consists of task number, task and completion time
+
+struct node *head = NULL, *cur, *newnode;
+//Some usful variables for the code
+void create() {   //Function to create a new node
     newnode = new node();
-    cout<<"Enter task: ";
-    getline(cin,newnode->task);
-    cout<<"Enter completion time: ";
-    cin>>newnode->CompletionTime;
-    cin.ignore();
-    newnode->next = NULL;
-}
-void addTask(){
+    newnode->i = ++n;
+    cout << "Task no. " << newnode->i << endl;
+    
+    cin.ignore();  // Clear the input buffer before using getline
+    cout << "Enter task: ";
+    getline(cin, newnode->task);
+    
+    cout << "Enter completion time: ";
+    cin >> newnode->CompletionTime;
+    
+    newnode->next = NULL;  
+}  
+
+void addTask() {   //Function to add a new task 
     create();
-    if(head == NULL){
+    if (head == NULL) { 
         head = newnode;
-    }
-    else{
+    } else {
         cur = head;
-        while(cur->next != NULL){
+        while (cur->next != NULL) {
             cur = cur->next;
         }
         cur->next = newnode;
     }
 }
-void deleteTask_byName(string name){
+
+void deleteTask_byNumber(int y) {    //Function to delete a task
     cur = head;
-    struct node *temp;
-    while( cur != NULL && cur->task != name){
+    node *temp = nullptr;
+    
+    while (cur != NULL && cur->i != y) {
         temp = cur;
         cur = cur->next;
     }
-    if(cur == NULL){
-        cout<<"Task not found"<<endl;
-    }
-    else if(cur == head){
+    
+    if (cur == NULL) {
+        cout << "Task not found" << endl;
+    } else if (cur == head) {
         temp = head;
         head = head->next;
         delete temp;
-    }
-    else if(cur->next == NULL){
+    } else if (cur->next == NULL) {
         temp->next = NULL;
         delete cur;
-    }
-    else{
+    } else {
         temp->next = cur->next;
         delete cur;
     }
 }
-void putDataIntoFile(){
-    ofstream outFile("todo.txt");  // Open file for writing
+
+void putDataIntoFile() {    //Function to save data into file
+    ofstream outFile("todo.txt", ios::trunc);  // Open file in trunc mode to clear previous contents
     if (outFile.is_open()) {
-        cur = head;
-        while (cur != NULL) {
-            outFile << cur->task << endl;
-            outFile << cur->CompletionTime << endl;
-            cur = cur->next;
+        if (head == NULL) {
+            cout << "No tasks to save. File will be cleared." << endl;
+        } else {
+            cur = head;
+            while (cur != NULL) {
+                outFile << cur->i << endl;
+                outFile << cur->task << endl;
+                outFile << cur->CompletionTime << endl;
+                cur = cur->next;
+            }
+            cout << "To-do list saved to file." << endl;
         }
         outFile.close();
-        cout << "To-do list saved to file." << endl;
     } else {
         cout << "Error opening file to write." << endl;
     }
 }
-void takeDataFromFile(){
-        ifstream inFile("todo.txt");  // Open file for reading
+
+
+void takeDataFromFile() {    //Function to load data from file
+    ifstream inFile("todo.txt");
     if (inFile.is_open()) {
         string task, completionTime;
+        int x;
         node* temp = nullptr;
-        
-        while (getline(inFile, task)) {          // Read the task
-            if (getline(inFile, completionTime)) { // Read the completion time
-                node* newNode = new node();       // Create a new node
-                newNode->task = task;
-                newNode->CompletionTime = completionTime;
-                newNode->next = nullptr;
-                
-                if (head == nullptr) {            // If list is empty, this is the first node
-                    head = newNode;
-                    temp = head;
-                } else {
-                    temp->next = newNode;         // Add to the end of the list
-                    temp = newNode;
-                }
+
+        while (inFile >> x) {  // Read the task number
+            inFile.ignore();   // Ignore leftover newline
+            getline(inFile, task);  // Read the task
+            getline(inFile, completionTime);  // Read the completion time
+            
+            node* newNode = new node();  // Create a new node
+            newNode->i = x;
+            newNode->task = task;
+            newNode->CompletionTime = completionTime;
+            newNode->next = nullptr;
+
+            if (head == nullptr) {  // If list is empty, this is the first node
+                head = newNode;
+                temp = head;
+            } else {
+                temp->next = newNode;  // Add to the end of the list
+                temp = newNode;
             }
         }
         inFile.close();
@@ -96,37 +119,39 @@ void takeDataFromFile(){
     } else {
         cout << "Error opening file to read." << endl;
     }
+}
 
-}   
-void display(){
-    if(head == NULL){
-        cout<<"List is empty"<<endl;
-    }
-    else{
+void display() {         //Function to display the list
+    if (head == NULL) {   
+        cout << "List is empty" << endl;
+    } else {
         cur = head;
-        while(cur->next != NULL){
-            cout<<cur->task<<" "<<cur->CompletionTime<<endl;
+        while (cur != NULL) {
+            cout <<cur->i<<": "<< cur->task << " __Finish by__  " << cur->CompletionTime << endl;
             cur = cur->next;
         }
-        cout<<cur->task<<" "<<cur->CompletionTime<<endl;
     }
 }
-int main(){
+
+int main() {            //Main function
     takeDataFromFile();
-    string name;
-    int ch;
-    while(1){
-        cout<<"Enter your choice: "<<endl;
-        cout<<"1. Add task ,2. Delete task ,3. display,4. exit"<<endl;
-        cin>>ch;
-        switch(ch){
+    int ch, q;
+    
+    while (1) {
+        cout << "Enter your choice: " << endl;
+        cout << "1. Add task, 2. Delete task, 3. Display, 4. Exit" << endl;
+        cin >> ch;
+        
+        switch (ch) {
             case 1: addTask();
                     break;
-            case 2: cout<<"Enter the task to delete: ";
-                    cin>>name;
-                    deleteTask_byName(name);
+            case 2: display();
+                    cout << "Enter the task number to delete: ";
+                    cin >> q;
+                    deleteTask_byNumber(q);
                     break;
-            case 3: display(); break;        
+            case 3: display();
+                    break;        
             case 4: putDataIntoFile();
                     exit(0);
         }
